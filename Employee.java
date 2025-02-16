fb29cc2f507b4bca8b81d9d35da8322db15a3695
sujanstha15 <raju40rr@gmail.com>
2025-02-15 22:49:33 -0600

    Testing

--- Employee.java
+++ Employee.java
@@ -3,4 +3,25 @@ public class Employee {
     private String lastName;
     private int employeeId;
     private String address;
+
+public Employee(String firstName, String lastName, int employeeId, String address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.employeeId  = employeeId;
+    this.address  = address;
+
+}
+
+public String toString(){
+    return firstName+" "+lastName+" "+employeeId+" "+ address;
+}
+//initializing default constructor
+    public Employee(){
+g
+    }
+//main method to test
+    public static void main(String[] args){
+    Employee e = new Employee("Sujan", "Shrestha", 15, "Monroe, Louisiana");
+    System.out.println(e);
+    }
 }
