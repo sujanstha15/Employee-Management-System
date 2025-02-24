da1b5be0dc971de0c81477cbcbad0b4b1ea49dc3
sujanstha15 <raju40rr@gmail.com>
2025-02-24 00:05:40 -0600

    mapping database with client and server

--- src/main/java/net/javaguide/ems/mapper/EmployeeMapper.java
+++ src/main/java/net/javaguide/ems/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package net.javaguide.ems.mapper;
 
 import net.javaguide.ems.dto.EmployeeDto;
 import net.javaguide.ems.entity.Employee;
+import lombok.Generated;
 
 public class EmployeeMapper {
     public static EmployeeDto mapToEmployeeDto(Employee employee){
@@ -9,7 +10,16 @@ public class EmployeeMapper {
                 employee.getId(),        // Correct reference
                 employee.getFirstName(), // Correct reference
                 employee.getLastName(),  // Correct reference
-                employee.getEmail()      // Fixed typo here (was: empployee.getEmail())
+                employee.getEmail()      // Fixed typo here (was: employee.getEmail())
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
         );
     }
 }
