0054b449e1524afdcfd451e032cd99bef7970ede
sujanstha15 <raju40rr@gmail.com>
2025-03-23 19:46:11 -0500

    Add Employee Form Handling in React App

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EmployeeComponent = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    // state varibles are above
+
+
+
+
+    function handleFirstName(e){
+        setFirstName(e.target.value);
+    }
+
+
+    function handleLastName(e){
+        setLastName(e.target.value);
+    }
+
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
+
+    function saveEmployee(e){
+        e.preventDefault();
+        const employee = {firstName, lastName, email};
+        console.log(employee);
+    }
+
   return (
-    <div>EmployeeComponent</div>
+    <div className='container'>
+        <div className='row'>
+        <div className='card'>
+            <h2 className='text-center'>Add Employee</h2>
+            <div className='card-body'>
+                <form>
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>FirstName</label>
+                        <input type="text" placeholder='Enter employee FirstName' name='firstName' value={firstName} className='form-control' onChange={handleFirstName}/>
+                    </div>
+
+                    {/* Last Name Comment */}
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Last Name:</label>
+                        <input type="text" placeholder='Enter employee Last Name' name='lastName' value={lastName} className='form-control' onChange={handleLastName}/>
+                    </div>
+                    {/* Email */}
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Email: </label>
+                        <input type="password" placeholder='Enter employee Email' name='email' value={email} className='form-control' onChange={handleEmail}/>
+                    </div>
+                    <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                </form>
+            </div>
+
+        </div>
+
+        </div>
+
+
+
+
+
+
+    </div>
   )
 }
 
