e80440c2201b351e675327073d5b3793ca3c407d
sujanstha15 <raju40rr@gmail.com>
2025-03-25 16:32:01 -0500

    Add employee button corrected

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -2,15 +2,11 @@ import React, { useState } from 'react'
 import { createEmployee } from '../services/EmployeeService'
 import {useNavigate} from 'react-router-dom'
 
-
 const EmployeeComponent = () => {
-
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
-    // state varibles are above
-
-    const [errors, setErrors] =useState({
+    const [errors, setErrors] = useState({
         firstName: '',
         lastName: '',
         email: ''
@@ -18,11 +14,9 @@ const EmployeeComponent = () => {
 
     const navigator = useNavigate();
 
-
     function handleFirstName(e){
         setFirstName(e.target.value);
     }
-
 
     function handleLastName(e){
         setLastName(e.target.value);
@@ -32,108 +26,105 @@ const EmployeeComponent = () => {
         setEmail(e.target.value);
     }
 
-    function saveEmployee(e){
-        e.preventDefault();
-
-        if(validateForm()){
-        const employee = {firstName, lastName, email};
-        console.log(employee);
-
-       createEmployee(employee).then((response) => {
-        console.log(response.data);
-        navigator('/employees');
-       })
-    }
-    
-    //javascript functino that will check the form data
     function validateForm(){
         let valid = true;
-        //error is nothing but a state variable
         const errorsCopy = {...errors};
 
         if(firstName.trim()){
-            errorsCopy.firstName ='';
+            errorsCopy.firstName = '';
         }
         else{
-            errorsCopy.firstName ='First name is required';
+            errorsCopy.firstName = 'First name is required';
             valid = false;
         }
 
         if(lastName.trim()){
-            errorsCopy.lastName ='';
+            errorsCopy.lastName = '';
         }   
         else{
-            errorsCopy.lastName ='Last name is required';
+            errorsCopy.lastName = 'Last name is required';
             valid = false;
         }
 
         if(email.trim()){            
-            errorsCopy.email ='';
+            errorsCopy.email = '';
         }
         else{
-            errorsCopy.email ='Email is required';
+            errorsCopy.email = 'Email is required';
             valid = false;
         }
         setErrors(errorsCopy);
         return valid;
-
     }
 
-  return (
-    <div className='container'>
-        <div className='row'>
-        <div className='card'>
-            <h2 className='text-center'>Add Employee</h2>
-            <div className='card-body'>
-                <form>
-                    <div className='form-group mb-2'>
-                        <label className='form-label'>FirstName</label>
-                        <input 
-                        type="text"
-                         placeholder='Enter employee FirstName'
-                          name='firstName' value={firstName} 
-                          className={`form-control ${ errors.firstName ? 'is-invalid' : ''}`} 
-                          onChange={handleFirstName}/>
-                          {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
-                        
-                    </div>
+    function saveEmployee(e){
+        e.preventDefault();
 
-                    {/* Last Name Comment */}
-                    <div className='form-group mb-2'>
-                        <label className='form-label'>Last Name:</label>
-                        <input type="text" 
-                        placeholder='Enter employee Last Name' 
-                        name='lastName' value={lastName} 
-                        className={`form-control ${ errors.lastName ? 'is-invalid' : ''}`} 
-                        onChange={handleLastName}/>
-                         {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
+        if(validateForm()){
+            const employee = {firstName, lastName, email};
+            console.log(employee);
+
+            createEmployee(employee).then((response) => {
+                console.log(response.data);
+                navigator('/employees');
+            }).catch(error => {
+                console.error('Error creating employee:', error);
+            });
+        }
+    }
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='card'>
+                    <h2 className='text-center'>Add Employee</h2>
+                    <div className='card-body'>
+                        <form>
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>FirstName</label>
+                                <input 
+                                    type="text"
+                                    placeholder='Enter employee FirstName'
+                                    name='firstName' 
+                                    value={firstName} 
+                                    className={`form-control ${ errors.firstName ? 'is-invalid' : ''}`} 
+                                    onChange={handleFirstName}
+                                />
+                                {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
+                            </div>
+
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>Last Name:</label>
+                                <input 
+                                    type="text" 
+                                    placeholder='Enter employee Last Name' 
+                                    name='lastName' 
+                                    value={lastName} 
+                                    className={`form-control ${ errors.lastName ? 'is-invalid' : ''}`} 
+                                    onChange={handleLastName}
+                                />
+                                {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
+                            </div>
+                            
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>Email: </label>
+                                <input 
+                                    type="text"
+                                    placeholder='Enter employee Email'
+                                    name='email' 
+                                    value={email} 
+                                    className={`form-control ${ errors.email ? 'is-invalid' : ''}`}
+                                    onChange={handleEmail}
+                                />
+                                {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
+                            </div>
+                            <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                        </form>
                     </div>
-                    {/* Email */}
-                    <div className='form-group mb-2'>
-                        <label className='form-label'>Email: </label>
-                        <input type="text"
-                         placeholder='Enter employee Email'
-                          name='email' 
-                          value={email} 
-                          className={`form-control ${ errors.email ? 'is-invalid' : ''}`}
-                          onChange={handleEmail}/>
-                          {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
-                    </div>
-                    <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
-                </form>
+                </div>
             </div>
-
         </div>
+    )
+}
 
-        </div>
-
-
-
-
-
-
-    </div>
-  )
-    
-}}
 export default EmployeeComponent;
