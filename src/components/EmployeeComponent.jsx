60a3de43b3c9e0a6af7657526d286e6c8b020150
sujanstha15 <raju40rr@gmail.com>
2025-03-24 22:52:37 -0500

    Add Employee Form Validation in React App

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -10,6 +10,12 @@ const EmployeeComponent = () => {
     const [email, setEmail] = useState('')
     // state varibles are above
 
+    const [errors, setErrors] =useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+
     const navigator = useNavigate();
 
 
@@ -29,6 +35,7 @@ const EmployeeComponent = () => {
     function saveEmployee(e){
         e.preventDefault();
 
+        if(validateForm()){
         const employee = {firstName, lastName, email};
         console.log(employee);
 
@@ -36,6 +43,40 @@ const EmployeeComponent = () => {
         console.log(response.data);
         navigator('/employees');
        })
+    }
+    
+    //javascript functino that will check the form data
+    function validateForm(){
+        let valid = true;
+        //error is nothing but a state variable
+        const errorsCopy = {...errors};
+
+        if(firstName.trim()){
+            errorsCopy.firstName ='';
+        }
+        else{
+            errorsCopy.firstName ='First name is required';
+            valid = false;
+        }
+
+        if(lastName.trim()){
+            errorsCopy.lastName ='';
+        }   
+        else{
+            errorsCopy.lastName ='Last name is required';
+            valid = false;
+        }
+
+        if(email.trim()){            
+            errorsCopy.email ='';
+        }
+        else{
+            errorsCopy.email ='Email is required';
+            valid = false;
+        }
+        setErrors(errorsCopy);
+        return valid;
+
     }
 
   return (
@@ -47,18 +88,36 @@ const EmployeeComponent = () => {
                 <form>
                     <div className='form-group mb-2'>
                         <label className='form-label'>FirstName</label>
-                        <input type="text" placeholder='Enter employee FirstName' name='firstName' value={firstName} className='form-control' onChange={handleFirstName}/>
+                        <input 
+                        type="text"
+                         placeholder='Enter employee FirstName'
+                          name='firstName' value={firstName} 
+                          className={`form-control ${ errors.firstName ? 'is-invalid' : ''}`} 
+                          onChange={handleFirstName}/>
+                          {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
+                        
                     </div>
 
                     {/* Last Name Comment */}
                     <div className='form-group mb-2'>
                         <label className='form-label'>Last Name:</label>
-                        <input type="text" placeholder='Enter employee Last Name' name='lastName' value={lastName} className='form-control' onChange={handleLastName}/>
+                        <input type="text" 
+                        placeholder='Enter employee Last Name' 
+                        name='lastName' value={lastName} 
+                        className={`form-control ${ errors.lastName ? 'is-invalid' : ''}`} 
+                        onChange={handleLastName}/>
+                         {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
                     </div>
                     {/* Email */}
                     <div className='form-group mb-2'>
                         <label className='form-label'>Email: </label>
-                        <input type="text" placeholder='Enter employee Email' name='email' value={email} className='form-control' onChange={handleEmail}/>
+                        <input type="text"
+                         placeholder='Enter employee Email'
+                          name='email' 
+                          value={email} 
+                          className={`form-control ${ errors.email ? 'is-invalid' : ''}`}
+                          onChange={handleEmail}/>
+                          {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                     </div>
                     <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
                 </form>
@@ -75,6 +134,6 @@ const EmployeeComponent = () => {
 
     </div>
   )
-}
-
+    
+}}
 export default EmployeeComponent;
