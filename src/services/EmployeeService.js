8c7b12dc88f74065af7b91fd40b0cdcd6e77634d
sujanstha15 <raju40rr@gmail.com>
2025-04-07 20:50:51 -0500

    Connect React App to Update Employee REST API

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -6,3 +6,6 @@ export const listEmployees = () =>  axios.get(REST_API_BASE_URL);
 
 export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
 
+//we have written REST client code to call update employee REST API using axios.put method
+
+export const updateEmployee = (employee, id) => axios.put(REST_API_BASE_URL + '/' + id, employee);
